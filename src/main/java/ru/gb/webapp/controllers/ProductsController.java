6d06b2c54ae70ac15b6af4851ef1ff645f7e0641
Kingl <siarheitsarik@gmail.com>
2022-05-28 15:34:58 +0300

    Homework Three

---
+++ src/main/java/ru/gb/webapp/controllers/ProductsController.java
@@ -0,0 +1,40 @@
+package ru.gb.webapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.gb.webapp.models.Product;
+import ru.gb.webapp.services.ProductService;
+
+@Controller
+public class ProductsController {
+
+    private ProductService service;
+
+    @Autowired
+    public ProductsController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/show_all")
+    public String showAllProducts(Model model){
+        model.addAttribute("products", service.findAll());
+        return "products";
+    }
+    @GetMapping(value = "/show/{id}")
+    public String showProductById(Model model,@PathVariable Long id){
+        model.addAttribute("product", service.getById(id));
+        return "product_info";
+    }
+    @GetMapping(value = "/create")
+    public String createProduct(){
+        return "create_product";
+    }
+
+    @PostMapping(value = "/create")
+    public String addProduct(@RequestParam Long id, @RequestParam String title, @RequestParam Double cost){
+        service.addProduct(new Product(id, title, cost));
+        return "redirect:/show_all";
+    }
+}
