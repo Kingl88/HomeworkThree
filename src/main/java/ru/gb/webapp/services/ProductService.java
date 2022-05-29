eff6f29453243e5cf1a63ec7a029ca5384944e1a
Kingl <siarheitsarik@gmail.com>
2022-05-29 23:54:41 +0300

    Homework Four

--- src/main/java/ru/gb/webapp/services/ProductService.java
+++ src/main/java/ru/gb/webapp/services/ProductService.java
@@ -15,15 +15,26 @@ public class ProductService {
     public ProductService(ProductRepository repository) {
         this.repository = repository;
     }
-public List<Product> findAll(){
-        return repository.findAll();
-}
 
-    public void addProduct(Product product){
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public void addProduct(Product product) {
         repository.addProduct(product);
     }
 
-    public Product getById(Long id){
-       return repository.getById(id);
+    public Product getById(Long id) {
+        return repository.getById(id);
+    }
+
+    public void costUp(Long id) {
+        repository.getById(id).setCost(repository.getById(id).getCost() + 1);
+    }
+
+    public void costDown(Long id) {
+        if(repository.getById(id).getCost() > 0) {
+            repository.getById(id).setCost(repository.getById(id).getCost() - 1);
+        }
     }
 }
